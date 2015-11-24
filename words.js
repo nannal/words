643cd36654d91a2b75dd9481206a9c59de3100ae
root <nannal@techious.com>
2015-11-24 09:27:39 +0000

    Cleanup button is broken

---
+++ words.js
@@ -0,0 +1,42 @@
+Tasks = new Mongo.Collection("tasks");
+Users = new Mongo.Collection("users");
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+  Template.body.events({
+    "submit .new-task": function (event) {
+      event.preventDefault();
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";
+    }});
+  Template.click.events({ 
+    "click .delete": function () {
+	event.preventDefault();
+      Meteor.call('removeAllPosts');
+    }
+  });
+//Template.footer.usercount = function () {
+//  return Meteor.users.find().count();
+//};
+
+}
+
+if (Meteor.isServer) {
+ Meteor.startup(function() {
+    return Meteor.methods({
+      removeAllPosts: function() {
+       return Tasks.remove({});
+      }
+    });
+  });
+
+
+}
